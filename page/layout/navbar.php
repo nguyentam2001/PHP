d1999f9762da8395bb8cd146a0765dad62443be9
Your Name <you@example.com>
2022-04-12 14:28:12 +0700

    Tam fix header error

--- page/layout/navbar.php
+++ page/layout/navbar.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
     <div class="navbar">
         <div class="navbar-icon">
         </div>
@@ -61,9 +50,3 @@
             </div>
         </div>
     </div>
-</body>
-<<<<<<< HEAD
-
-=======
->>>>>>> 03ad2f9a9df4ea5ebec4a1c643f5fbd3a96f763c
-</html>
