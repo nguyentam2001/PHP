584ba78166d1ab52d19449796f377dbae9979cd4
Your Name <you@example.com>
2022-05-16 14:42:37 +0700

    Tam Update

---
+++ frontend/product.php
@@ -0,0 +1,307 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+            integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ=="
+            crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="shortcut icon" href="images/favicon.ico" />
+        <link rel="icon" href="images/favicon.ico" />
+        <title>ntstore</title>
+        <link rel="stylesheet" href="./assest/css/base.css" />
+        <link rel="stylesheet" href="./assest/css/product.css" />
+
+        <link rel="stylesheet" href="./assest/css/grid.css" />
+    </head>
+
+    <body>
+        <div class="main">
+            <div class="grid wide">
+                <div class="content">
+                    <div class="row">
+                        <div class="link-shop-background col l-12">
+                            <ul class="link-shop">
+                                <li class="link-shop-item">
+                                    <a href="./index.php" class="link-shop-item_link access">NTstore</a>
+                                    <i class="fas fa-chevron-right"></i>
+                                </li>
+                                <li class="link-shop-item">
+                                    <a href="#" class="link-shop-item_link">Đồ công nghệ</a>
+                                    <i class="fas fa-chevron-right"></i>
+                                </li>
+                                <li class="link-shop-item">
+                                    <a href="#" class="link-shop-item_link">Flycam Dji</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <!-- container-product -->
+                    <div class="container-item row">
+                        <div class="product-item col l-5">
+                            <div class="product-item__img" style="
+                background-image: url(https://cf.shopee.vn/file/68fff1255e2a9fa953384383ecfb937c_tn);
+              "></div>
+                            <div class="product-item-list">
+
+                            </div>
+                            <div class="social-favorite">
+                                <span class="social">
+                                    Chia sẻ:
+                                    <i class="fab fa-facebook-messenger social-icon"></i>
+                                    <i class="fab fa-facebook social-icon"></i>
+                                    <i class="fab fa-pinterest social-icon"></i>
+                                    <i class="fab fa-twitter-square social-icon"></i>
+                                </span>
+
+                                <span class="favorite">
+                                    <i class="far fa-heart favorite-icon"></i>
+                                    Đã thích (249)
+
+                                </span>
+                            </div>
+                        </div>
+                        <div class="col l-7">
+                            <div class="row">
+                                <div class="col l-12">
+                                    <div class="item-body">
+                                        <div class="item-body-header">
+                                            <span class="home-product-item__favourite">
+                                                <span>Yêu thích</span>
+                                            </span>
+
+                                            <p class="item-body_des">
+                                                Flycam Q8 Gps cảm biến quang - động cơ không chổi than -
+                                                Hàng giá rẻ
+                                            </p>
+                                        </div>
+                                        <div class="item-body_status">
+                                            <div class="item-body_status-start item-body_status-content">
+                                                <span class="status-start-rate">4/5</span>
+                                                <i class="home-product-item__star--gold fas fa-star"></i>
+                                                <i class="home-product-item__star--gold fas fa-star"></i>
+                                                <i class="home-product-item__star--gold fas fa-star"></i>
+                                                <i class="home-product-item__star--gold fas fa-star"></i>
+                                            </div>
+                                            <span class="item-body_status-review item-body_status-content">
+                                                <span class="status-review">4</span> Đánh Giá
+                                            </span>
+                                            <span class="item-body_status-sell item-body_status-content">
+                                                <span class="status-sell">9</span> Đã Bán
+                                            </span>
+                                        </div>
+
+                                        <div class="item-body__price">
+                                            <span class="product-item__price-old">980.000đ</span>
+                                            <span class="product-item__price-current">580.000đ</span>
+                                            <span class="product-item__price-percent"> 43% giảm</span>
+                                        </div>
+
+                                        <div class="item-body__oder">
+                                            <span class="item-body__oder_deal-header item-body__oder-title">
+                                                Deal Sốc
+                                            </span>
+                                            <span class="
+                        item-body__oder_deal-content item-body__oder-content
+                      ">
+                                                Mua kèm Deal Sốc
+                                            </span>
+                                        </div>
+                                        <div class="item-body__oder">
+                                            <span class="
+                        item-body__oder-title item-body__oder-title-address
+                      ">
+                                                Vận chuyển
+                                            </span>
+                                            <div class="
+                        item-body__oder-content item-body__oder-content-address
+                      ">
+                                                <div class="content__address">
+                                                    <i class="fas fa-car-side content__address-icon-car"></i>
+                                                    <span>Vận chuyển tới</span>
+                                                    <a href="#" class="content__address-home">Pường Minh Khai, Quận Bắc
+                                                        Từ Liêm
+                                                        <i class="fas fa-chevron-down"></i>
+                                                    </a>
+                                                </div>
+
+                                                <div class="content__transport">
+                                                    <span>Phí vận chuyển</span>
+                                                    <a href="#" class="content__transport-price">Không hỗ trợ
+
+                                                        <i class="fas fa-chevron-down"></i>
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="item-body__oder">
+                                            <span class="
+                        item-body__oder-title item-body__oder-title-classify
+                      ">
+                                                Loại
+                                            </span>
+                                            <div class="
+                        item-body__oder-content item-body__oder-content-classify
+                      ">
+                                                <div class="content-classify__btn content-classify__btn-one">
+                                                    Flycam 1 Pin
+                                                </div>
+                                                <div class="content-classify__btn content-classify__btn-two">
+                                                    Flycam 2 Pin
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="item-body__oder">
+                                            <span class="item-body__amount-title item-body__oder-title">
+                                                Số lượng
+                                            </span>
+                                            <div class="
+                         item-body__oder-content item-body__content-amount
+                      ">
+                                                <span class="item-body__oder-amount item-body__oder-minus">-</span>
+                                                <span class="item-body__oder-amount item-body__oder-current">1</span>
+                                                <span class="item-body__oder-amount item-body__oder-plus">+</span>
+                                            </div>
+                                        </div>
+
+                                        <div class="item-body__oder">
+                                            <span class="item-body__btn-add-cart btn-border">
+                                                <i class="fas fa-cart-plus"></i>
+                                                Thêm vào giỏ hàng
+                                            </span>
+                                            <button class="btn-primary btn-oder">Mua Ngay</button>
+                                        </div>
+                                        <hr>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="content-shop row">
+                        <div class="col l-5">
+                            <div class="sell-info ">
+                                <a href="#"><img src="./assest/img/img_vartar/sell-user.png" alt=""
+                                        class="sell-info-img"></a>
+                                <div class="sell-info-content">
+                                    <div class="sell-info-name">khanhkhanh78910</div>
+                                    <div class="sell-info-status">
+                                        <i class="fas fa-clock sell-info-status-clock"></i>Đang nghỉ bán
+                                    </div>
+                                    <div class="sell-info-btn-container">
+                                        <div class="sell-info-chat btn-border ">
+                                            <i class="far fa-comment-dots sell-info-chat-icon"></i>Chát ngay
+                                        </div>
+                                        <div class="sell-info-check-shop btn-border">
+                                            <i class="fas fa-store-alt"></i>
+                                            Xem shop
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class=" l-7">
+                            <div class="sell-ratio row">
+                                <div class="sell-ratio-content col l-4">
+                                    <div class="sell-ratio-vote">
+                                        Đánh giá <span class="sell-ratio-vote_amount">36</span>
+                                    </div>
+                                    <div class="sell-ratio-vote">
+                                        Sản Phẩm <span class="sell-ratio-vote_amount">12</span>
+                                    </div>
+                                </div>
+                                <div class="sell-ratio-content col l-4">
+                                    <div class="sell-ratio-vote">
+                                        Tỷ lệ phản hồi <span class="sell-ratio-vote_amount">24%</span>
+                                    </div>
+                                    <div class="sell-ratio-vote">
+                                        Thời gian phản hồi <span class="sell-ratio-vote_amount">Trong vài giờ</span>
+                                    </div>
+                                </div>
+                                <div class="sell-ratio-content col l-4">
+                                    <div class="sell-ratio-vote">
+                                        Tham gia <span class="sell-ratio-vote_amount">8 tháng trước</span>
+                                    </div>
+                                    <div class="sell-ratio-vote">
+                                        Người Theo dõi <span class="sell-ratio-vote_amount">10</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="product-content col l-12">
+                            <div class="product-details">
+                                <span class="product-details-title">
+                                    Chi tiết sản phẩm
+                                </span>
+
+                                <div class="row product-details-container ">
+                                    <div class="col l-1">
+                                        <span class="product-details-cate">Danh mục</span>
+                                    </div>
+                                    <div class="col l-11">
+                                        <ul class="link-shop">
+                                            <li class="link-shop-item">
+                                                <a href="#" class="link-shop-item_link access">NTstore</a>
+                                                <i class="fas fa-chevron-right"></i>
+                                            </li>
+                                            <li class="link-shop-item">
+                                                <a href="#" class="link-shop-item_link">Đồ công nghệ</a>
+                                                <i class="fas fa-chevron-right"></i>
+                                            </li>
+                                            <li class="link-shop-item">
+                                                <a href="#" class="link-shop-item_link">Flycam Dji</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+
+                                <div class="row product-details-container">
+                                    <div class="col l-1">
+                                        <span class="product-detail-store">Kho hàng</span>
+                                    </div>
+                                    <div class="col l-11">
+                                        <span class="product-detail-store__sum">95</span>
+                                    </div>
+                                </div>
+
+                                <div class="row product-details-container">
+                                    <div class="col l-1">
+                                        <span class="product-detail-from">Gửi từ</span>
+                                    </div>
+                                    <div class="col l-11">
+                                        <span class="product-detail-from__address">Thành Phố Nha Trang, Khánh Hòa</span>
+                                    </div>
+                                </div>
+                                <span class="product-details-title product-description">
+                                    Mô tả sản phẩm
+                                </span>
+
+                                <p class="product-description-details">
+                                    _Trọng lượng máy bay: 94,8g (có pin)
+                                    _ Máy ảnh: 2MP góc rộng 720P, 0,3MP (Tùy chọn) <br>
+                                    _ Kích thước: 27x19,5x5cm (cánh máy bay không gập lại) và 12,5x7,5x5cm (cánh máy bay
+                                    gập lại)<br>
+                                    _ Trọng lượng gói hàng: 360g, có tay cầm có thể gập lại, kích thước nhỏ, dễ mang
+                                    theo. <br>
+                                    _ Chức năng: chế độ giữ độ cao giúp chuyến bay ổn định. Có wifi có thể kết nối app,
+                                    hệ thống apk để chụp ảnh, quay phim, truyền tải thời gian thực thông qua hình ảnh
+                                    camera điện thoại. Công nghệ 2.4GHz được sử dụng để chống nhiễu. Có thể bay lên,
+                                    xuống, tiến, lùi, bay sang trái, bay sang phải và xoay 360 °. Thân máy bay
+                                    quadcopter được làm bằng nhựa kỹ thuật chịu lực và chịu lực cao, khả năng chống chịu
+                                    nhẹ và bền.<br>
+                                    _ Bao gồm: 1 Eachine E58 RC Quadcopter, 1 Máy phát, 1 Pin Lipo 3.7V 500mAh, 1 Cáp
+                                    sạc USB, 4 Nắp bảo vệ , 4 Phụ tùng, 1 tua vít, 1 Hướng dẫn sử dụng<br>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </body>
+
+</html>
